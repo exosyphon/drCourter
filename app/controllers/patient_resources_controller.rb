02f66224e9fc7b08bd636055b4c7f1dcffb9ef5e
exosyphon <exosyphon@yahoo.com>
2015-08-16 18:42:28 -0600

    Nested submenus FTW

--- app/controllers/patient_resources_controller.rb
+++ app/controllers/patient_resources_controller.rb
@@ -1,4 +1,7 @@
 class PatientResourcesController < ApplicationController
   def index
   end
+
+  def new_patient_forms
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/our_practice', to: 'our_practice#index'
   get '/about_us', to: 'about_us#index'
   get '/patient_resources', to: 'patient_resources#index'
+  get '/patient_resources/new_patient_forms', to: 'patient_resources#new_patient_forms'
   get '/contact_us', to: 'contact_us#index'
   get '/services', to: 'services#index'
   get '/our_technology', to: 'our_technology#index'
