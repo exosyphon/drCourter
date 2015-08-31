1037d46e6d6762028f12822ed35b008cac0ca5ad
exosyphon <exosyphon@yahoo.com>
2015-08-30 23:06:11 -0600

    Remove sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users#, path_names: {sign_up: ''}
+  devise_for :users, path_names: {sign_up: ''}
   resources :text_logs do
     post '/send_text', to: 'text_logs#send_text'
     get '/view_audits', to: 'text_logs#view_audits', on: :collection
